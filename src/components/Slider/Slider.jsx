4a24e80dce86b2966e32727a4038d89c4f5e8498
Player3601314 <159936023+Player3601314@users.noreply.github.com>
2024-07-21 02:34:29 +0500

    added slider imgs

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -20,16 +20,16 @@ export const Slider = () => {
       className='w-[90%] m-auto h-[80vh] sm:h-[40vh] md:h-[40vh]'
     >
       <SwiperSlide className='w-[100%] h-[100%] rounded-[18px]'>
-        <img className='w-[100%] h-[100%] object-cover rounded-[18px]' src="http://look.uz/assets/homepage-slider-1-98ea9b48.jpg" alt="" />
+        <img className='w-[100%] h-[100%] object-cover rounded-[18px]' src="http://loook.uz/assets/homepage-slider-1-98ea9b48.jpg" alt="" />
       </SwiperSlide>
       <SwiperSlide className='w-[100%] h-[100%] rounded-[18px]'>
-        <img className='w-[100%] h-[100%] object-cover rounded-[18px]' src="http://look.uz/assets/homepage-slider-2-058ad017.jpg" alt="" />
+        <img className='w-[100%] h-[100%] object-cover rounded-[18px]' src="http://loook.uz/assets/homepage-slider-2-058ad017.jpg" alt="" />
       </SwiperSlide>
       <SwiperSlide className='w-[100%] h-[100%] rounded-[18px]'>
-        <img className='w-[100%] h-[100%] object-cover rounded-[18px]' src="http://look.uz/assets/homepage-slider-3-b840f884.jpg" alt="" />
+        <img className='w-[100%] h-[100%] object-cover rounded-[18px]' src="http://loook.uz/assets/homepage-slider-3-b840f884.jpg" alt="" />
       </SwiperSlide>
       <SwiperSlide className='w-[100%] h-[100%] rounded-[18px]'>
-        <img className='w-[100%] h-[100%] object-cover rounded-[18px]' src="http://look.uz/assets/homepage-slider-4-3b760d92.jpg" alt="" />
+        <img className='w-[100%] h-[100%] object-cover rounded-[18px]' src="http://loook.uz/assets/homepage-slider-4-3b760d92.jpg" alt="" />
       </SwiperSlide>
     </Swiper>
   );
